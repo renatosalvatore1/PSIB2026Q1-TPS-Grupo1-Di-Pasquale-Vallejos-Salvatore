7eff1a244b64217b6ff385b20d716587685cbf02
Renato Salvatore <rsalvatore@itba.edu.ar>
2026-05-26 17:34:22 -0300

    visualizacion del PSD y aplico filtro notch para eliminar ruido de linea y ruido raro cada 32 Hz

--- notebooks_senales/pruebas_nato.py
+++ notebooks_senales/pruebas_nato.py
@@ -1,6 +1,9 @@
 import os
 import gdown
 from pathlib import Path
+import mne
+import matplotlib.pyplot as plt
+import numpy as np
 
 # ─── Configuración ────────────────────────────────────────────────
 FOLDER_URL = "https://drive.google.com/drive/folders/1fzN5R6Bzm3g0PjLOplewveW8wo181J60?usp=drive_link"
@@ -22,3 +25,47 @@ if not any(DATA_DIR.iterdir()):
 else:
     n = len(list(DATA_DIR.iterdir()))
     print(f"✓ {DATA_DIR} ya tiene {n} archivo(s), salteando descarga.")
+
+raw = mne.io.read_raw_bdf(input_fname='./data/sub-050_task-med2_eeg.bdf', preload=True)
+eeg_channels = raw.ch_names[:64]
+#Me quedo solo con los EEG
+raw_eeg=raw.copy().pick(eeg_channels)
+
+psds, freqs = mne.time_frequency.psd_array_welch(
+    raw_eeg.get_data(), 
+    sfreq=1024, 
+    n_fft=4096,
+    n_per_seg=4096, 
+    output="power")
+
+
+
+plt.plot(freqs,psds[30])
+plt.semilogy()
+plt.xlim(0,500)
+plt.show()
+
+#Aplico filtro nothc para sacar ruido de linea >50Hz
+
+filtrado = mne.filter.notch_filter(
+    raw_eeg.get_data()[30],
+    Fs=1024,
+    freqs=np.arange(48, 512, 32),
+    notch_widths = None,
+    method = 'fir'
+)
+
+psds_f, freqs_f = mne.time_frequency.psd_array_welch(
+    filtrado, 
+    sfreq=1024, 
+    n_fft=4096,
+    n_per_seg=4096, 
+    output="power")
+
+plt.plot(freqs_f, psds_f)
+plt.semilogy()
+plt.xlim(0,500)
+plt.show()
+
+
+
