3b5cec05ddb4ffff75cfc0f28c56fd29e8e6d16d
Renato Salvatore <rsalvatore@itba.edu.ar>
2026-05-30 18:47:38 -0300

    Arranque el analisis PSD

--- notebooks_senales/pruebas_nato.py
+++ notebooks_senales/pruebas_nato.py
@@ -4,6 +4,8 @@ from pathlib import Path
 import mne
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
+from mne.viz import plot_filter, plot_ideal_filter
 
 # ─── Configuración ────────────────────────────────────────────────
 FOLDER_URL = "https://drive.google.com/drive/folders/1fzN5R6Bzm3g0PjLOplewveW8wo181J60?usp=drive_link"
@@ -26,32 +28,37 @@ else:
     n = len(list(DATA_DIR.iterdir()))
     print(f"✓ {DATA_DIR} ya tiene {n} archivo(s), salteando descarga.")
 
-raw = mne.io.read_raw_bdf(input_fname='./data/sub-050_task-med2_eeg.bdf', preload=True)
+raw = mne.io.read_raw_bdf(input_fname='./data/sub-070_task-med2_eeg.bdf', preload=True)
 eeg_channels = raw.ch_names[:64]
 #Me quedo solo con los EEG
 raw_eeg=raw.copy().pick(eeg_channels)
 
+#aplicamos pasabajos con fc=100 Hz
+eeg_filtrado=raw_eeg.filter(l_freq=None, h_freq=100)
+
 psds, freqs = mne.time_frequency.psd_array_welch(
-    raw_eeg.get_data(), 
+    eeg_filtrado.get_data(), 
     sfreq=1024, 
     n_fft=4096,
+    fmax=100,
     n_per_seg=4096, 
     output="power")
 
-
-
+a= np.arange(49, 512, 32) 
+b= np.arange(49,512,50)
+freqs_notch = (np.concatenate((a,[49.7]), axis=None))
 plt.plot(freqs,psds[30])
 plt.semilogy()
-plt.xlim(0,500)
+plt.xlim(0,100)
 plt.show()
 
 #Aplico filtro nothc para sacar ruido de linea >50Hz
 
 filtrado = mne.filter.notch_filter(
-    raw_eeg.get_data()[30],
+    eeg_filtrado.get_data()[30],
     Fs=1024,
-    freqs=np.arange(48, 512, 32),
-    notch_widths = None,
+    freqs=a,
+    notch_widths = 2,
     method = 'fir'
 )
 
@@ -59,13 +66,23 @@ psds_f, freqs_f = mne.time_frequency.psd_array_welch(
     filtrado, 
     sfreq=1024, 
     n_fft=4096,
+    fmax=100,
     n_per_seg=4096, 
     output="power")
 
 plt.plot(freqs_f, psds_f)
-plt.semilogy()
-plt.xlim(0,500)
+plt.loglog()
+plt.xlim(0,100)
 plt.show()
+
+#ANALISIS PSD POR
+bandas = [[1,4],[4, 8], [8,13],[13,30]]
+
+for i in range(len(bandas)):
+    mask = (freqs_f >= bandas[i][0]) & (freqs_f <= bandas[i][1])
+    band_power = np.trapezoid(psds_f[mask],freqs_f[mask])
+    print(band_power)
+
 
 
 
